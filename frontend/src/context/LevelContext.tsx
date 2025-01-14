62bab0c732081a23724aa07c395cfbc160ab7821
Alex Becci <alex.becci@bingopilar.corp>
2025-01-14 13:00:39 -0300

    prod

--- frontend/src/context/LevelContext.tsx
+++ frontend/src/context/LevelContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import  { createContext, useContext, useState, ReactNode } from 'react';
 
 // Definir el tipo para el contexto
 interface LevelContextType {
